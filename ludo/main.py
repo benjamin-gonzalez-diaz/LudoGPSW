f07a54c1851421a6b808e0e28eac0c6690363a47
BenjaGonzalez <Bigonzalez@miuandes.cl>
2023-09-01 20:31:08 -0400

    flujo

--- ludo/main.py
+++ ludo/main.py
@@ -9,6 +9,14 @@ play = False
 def lanzar_dado():
     return random.randint(1, 6)
 
+def desglose(lists, opt):
+    if(opt == 1):
+        segundos_valores = [sublista[1] for sublista in lists]
+        return segundos_valores
+    if(opt == 0):
+        segundos_valores = [sublista[0] for sublista in lists]
+        return segundos_valores
+
 def generar_lista_jugadores_y_dados(num_jugadores,opt):
     list_player = []
     list_dice = []
@@ -33,32 +41,17 @@ def generar_lista_jugadores_y_dados(num_jugadores,opt):
 
     return list_player_and_dice, list_dice, list_player
 
-def reemplazar_empates(list_dice, list_player_And_dice, list_order, jugadores):
-    empates = [i for i in range(7) if list_dice.count(i) > 1]
-
-    for i in empates:
-        for j in range(jugadores):
-            if list_player_And_dice[j][1] == i:
-                print("El jugador", list_player_And_dice[j][0], "vuelve a tirar")
-                list_order.append(list_player_And_dice[j][0])
-
-    print("list_order:", list_order)
-    list_player2 = list_player_And_dice.copy()
-
-    for i in list_order:
-        list_player2 = [player for player in list_player2 if player[0] != i]
-
-    all2 = generar_lista_jugadores_y_dados(list_order, 0)
-    new_dice = all2[0]
-
-    for i in range(len(list_player_And_dice)):
-        for j in range(len(list_order)):
-            if list_player_And_dice[i][0] == list_order[j]:
-                list_player_And_dice.remove(list_player_And_dice[i])
-                list_player_And_dice.append(new_dice[j])
-
-    return list_player_And_dice
-
+def reemplazar_empates( list_player_And_dice, jugadores):
+    ald = []
+    listDice = desglose(list_player_And_dice)
+    for i in listDice:
+        if(listDice.count(i) > 1):
+            for j in range(jugadores):
+                if (list_player_And_dice[j][1] == i):
+                    print("el jugador ", list_player_And_dice[j][0]," vuelve a tirar")
+                    ald.append(list_player_And_dice[j][0])
+    
+    return ald
 class Jugador:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -84,16 +77,12 @@ list_player = all[2]
 print("list_dice", list_dice)
 for i in range(7):
     if(list_dice.count(i) > 1):
-        play = False
         for j in range(jugadores):
             if (list_player_And_dice[j][1] == i):
                 print("el jugador ", list_player_And_dice[j][0]," vuelve a tirar")
                 list_order.append(list_player_And_dice[j][0])
-            else:
-                play = True
 
 
-print(play)
 print("list_order: ",list_order)
 print("list_player", list_player)
 list_player2 = list_player
@@ -107,8 +96,9 @@ new_dice = all2[0]
 print("list_dice",list_dice)
 print("list_player",list_player)
 print("list_player_And_dice", list_player_And_dice)
-lisasd = list_player_And_dice
 
+who_start = desglose(list_player_And_dice,1)
+print(who_start)
 for i in range(len(list_player_And_dice)):
 
     for j in range(len(list_order)):
@@ -116,6 +106,27 @@ for i in range(len(list_player_And_dice)):
                 list_player_And_dice.remove(list_player_And_dice[i])
                 list_player_And_dice.append(new_dice[j])
 
-
+who_start = desglose(list_player_And_dice,1)
+who_start = sorted(who_start)
+print(who_start)
 print("list_player_And_dice", list_player_And_dice)
 
+# Ordena la lista en función de los valores en la posición [x][1]
+lista_ordenada = sorted(list_player_And_dice, key=lambda x: x[1], reverse=True)
+
+# Imprime la lista ordenada
+print("lista_ordenada", lista_ordenada)
+turno = 0
+while True:
+
+    x = input()
+
+    start = desglose(lista_ordenada,0)
+
+    print("turno del jugador    n° ", start[turno])    
+
+    if(turno == len(start) -1):
+        turno = 0
+    else:
+        turno += 1
+    
